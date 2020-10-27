9784ce05f65517c024b4bdc061cce2d9ea8f7123
tomcat <2889567356@qq.com>
2020-10-28 04:37:58 +0800

    product_02

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -1,11 +1,209 @@
 import React, {Component} from "react";
+import { Card , Input ,Form,InputNumber, Button,DatePicker,Cascader,Upload, message} from 'antd';
+import { ArrowLeftOutlined , HeartTwoTone,LoadingOutlined, PlusOutlined} from '@ant-design/icons';
+
+const { TextArea } = Input;
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+
+const validateMessages = {
+    required: '',
+};
+//上传图片配置
+function getBase64(img, callback) {
+
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+}
+
 
 export default class ProductUpdate extends Component {
+    state = {
+        loading: false,
+    };
+
+
     render() {
-        return (
+        const title=(
+            <span>
+                <a onClick={() => {this.props.history.push("/product")}} style={{marginRight:20}}><ArrowLeftOutlined /></a>
+                添加商品
+            </span>
+        )
+        const { loading } = this.state;
+        const uploadButton = (
             <div>
-                ProductUpdate
+                {<PlusOutlined />}
+                <div style={{ marginTop: 8 }}>Upload</div>
             </div>
+        );
+
+        const onFinish = values => {
+            this.props.history.push("/product",values)
+        };
+
+        return (
+            <Card title={title} style={{ width: "100%"}}>
+                <Form style={{width:"40%"}} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                    <Form.Item name="name" label="商品名称" rules={[{ required: true , message: '必填项不能为空'}]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="miaoshu" label="商品描述" rules={[{ required: true, message: '必填项不能为空' }]}>
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item name="jiage" label="商品价格" rules={[{ required: true , message: '必填项不能为空'}]} style={{width:"70%"}} help="单位:元">
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item name="replace" label="商品分类" rules={[{ required: true, message: '必填项不能为空' }]} >
+                        <Cascader
+                            options={[
+                                {
+                                    value: '食品',
+                                    label: '食品',
+                                    children: [
+                                        {
+                                            value: '肉食',
+                                            label: '肉食',
+                                            children:[
+                                                {
+                                                    value: '牛肉',
+                                                    label: '牛肉',
+                                                },
+                                                {
+                                                    value: '猪肉',
+                                                    label: '猪肉',
+                                                },
+                                                {
+                                                    value: '狗肉',
+                                                    label: '狗肉',
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            value: '蔬菜',
+                                            label: '蔬菜',
+                                            children:[
+                                                {
+                                                    value: '土豆',
+                                                    label: '土豆',
+                                                },
+                                                {
+                                                    value: '白菜',
+                                                    label: '白菜',
+                                                },
+                                                {
+                                                    value: '萝卜',
+                                                    label: '萝卜',
+                                                },
+                                            ]
+                                        },
+                                    ],
+                                },
+                                {
+                                    value: '玩具',
+                                    label: '玩具',
+                                    children: [
+                                        {
+                                            value: '飞机',
+                                            label: '飞机',
+                                            children:[
+                                                {
+                                                    value: '牛肉',
+                                                    label: '牛肉',
+                                                },
+                                                {
+                                                    value: '猪肉',
+                                                    label: '猪肉',
+                                                },
+                                                {
+                                                    value: '狗肉',
+                                                    label: '狗肉',
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            value: '汽车',
+                                            label: '汽车',
+                                            children:[
+                                                {
+                                                    value: '土豆',
+                                                    label: '土豆',
+                                                },
+                                                {
+                                                    value: '白菜',
+                                                    label: '白菜',
+                                                },
+                                                {
+                                                    value: '萝卜',
+                                                    label: '萝卜',
+                                                },
+                                            ]
+                                        },
+                                    ],
+                                },
+                                {
+                                    value: '家具',
+                                    label: '家具',
+                                    children: [
+                                        {
+                                            value: '桌子',
+                                            label: '桌子',
+                                            children:[
+                                                {
+                                                    value: '牛肉',
+                                                    label: '牛肉',
+                                                },
+                                                {
+                                                    value: '猪肉',
+                                                    label: '猪肉',
+                                                },
+                                                {
+                                                    value: '狗肉',
+                                                    label: '狗肉',
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            value: '椅子',
+                                            label: '椅子',
+                                            children:[
+                                                {
+                                                    value: '土豆',
+                                                    label: '土豆',
+                                                },
+                                                {
+                                                    value: '白菜',
+                                                    label: '白菜',
+                                                },
+                                                {
+                                                    value: '萝卜',
+                                                    label: '萝卜',
+                                                },
+                                            ]
+                                        },
+                                    ],
+                                },
+                            ]} placeholder="请选择分类"
+                        />
+                    </Form.Item>
+                    <Form.Item name="image" label="商品图片" style={{paddingLeft:10}}>
+                        <Upload
+                            listType="picture-card"
+                            className="avatar-uploader"
+                        >
+                            {uploadButton}
+                        </Upload>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 5 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         )
     }
 }
--- src/pages/product/home.jsx
+++ src/pages/product/home.jsx
@@ -1,30 +1,211 @@
 import React, {Component} from "react";
-import { Select,Card,Input,Button,Form,Table, Pagination  } from 'antd';
+import { Select,Card,Input,Button,Table, Pagination  } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-
 const { Option } = Select;
-const { Search } = Input;
-const Item=Form.Item;
 
 const onSearch = value => console.log(value);
 export default class ProductHome extends Component {
+
+
+    values=this.props.location.state;
+
     state={
-        searchName:"0",
-        searchType:"",
+        searchName:"",        //搜索的值
+        searchType:"0",     //搜索类型
+        data : [
+            {
+                key: 1,
+                name: 'John Brown',
+                datenow:"1",
+                miaoshu: 'New York No. 1 Lake Park',
+                jiage:1888+"$",
+                image:require("./images/1.jpg"),
+                description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+            },
+            {
+                key: 2,
+                name: 'Jim Green',
+                datenow:"1",
+                miaoshu: 'London No. 1 Lake Park',
+                jiage:1388+"$",
+                image:require("./images/2.jpg"),
+                description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+            },
+            {
+                key: 3,
+                name: 'Not Expandable',
+                datenow:"1",
+                miaoshu: 'Jiangsu No. 1 Lake Park',
+                jiage:2888+"$",
+                image:require("./images/3.jpg"),
+                description: 'This not expandable',
+            },
+            {
+                key: 4,
+                name: 'Joe Black',
+                datenow:"1",
+                miaoshu: 'Sidney No. 1 Lake Park',
+                jiage:6888+"$",
+                image:require("./images/4.jpg"),
+                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+            },
+            {
+                key: 5,
+                name: 'Joe Black',
+                datenow:"1",
+                miaoshu: 'Sidney No. 1 Lake Park',
+                jiage:5888+"$",
+                image:require("./images/5.jpg"),
+                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+            },
+            {
+                key: 6,
+                name: 'Joe Black',
+                datenow:"1",
+                miaoshu: "Sidney No. 1 Lake Park32",
+                jiage:4888+"$",
+                image:require("./images/6.jpg"),
+                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+            },
+        ],     //搜索数据
+        redata:[
+            {
+                key: 1,
+                name: 'John Brown',
+                datenow:"1",
+                miaoshu: 'New York No. 1 Lake Park',
+                jiage:1888+"$",
+                image:require("./images/1.jpg"),
+                description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+            },
+            {
+                key: 2,
+                name: 'Jim Green',
+                datenow:"1",
+                miaoshu: 'London No. 1 Lake Park',
+                jiage:1388+"$",
+                image:require("./images/2.jpg"),
+                description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+            },
+            {
+                key: 3,
+                name: 'Not Expandable',
+                datenow:"1",
+                miaoshu: 'Jiangsu No. 1 Lake Park',
+                jiage:2888+"$",
+                image:require("./images/3.jpg"),
+                description: 'This not expandable',
+            },
+            {
+                key: 4,
+                name: 'Joe Black',
+                datenow:"1",
+                miaoshu: 'Sidney No. 1 Lake Park',
+                jiage:6888+"$",
+                image:require("./images/4.jpg"),
+                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+            },
+            {
+                key: 5,
+                name: 'Joe Black',
+                datenow:"1",
+                miaoshu: 'Sidney No. 1 Lake Park',
+                jiage:5888+"$",
+                image:require("./images/5.jpg"),
+                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+            },
+            {
+                key: 6,
+                name: 'Joe Black',
+                datenow:"1",
+                miaoshu: "Sidney No. 1 Lake Park32",
+                jiage:4888+"$",
+                image:require("./images/6.jpg"),
+                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+            },
+        ],    //静态数据
+    }
+    //添加商品
+    getProduct = () =>{
+
+        const {data}=this.state;
+        const result=data;
+
+        if(this.values){
+            const dateadd={
+                key: data.length+1,
+                name: this.values.name,
+                datenow:"1",
+                miaoshu: this.values.miaoshu,
+                jiage:this.values.jiage+"$",
+                image:require("./images/"+this.values.image.file.name),
+                description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+            }
+            result.push(dateadd);
+        } else{
+            return "";
+        }
+        this.setState({
+            data:result
+        })
+    }
+    //搜索
+    onSearchUp = () => {
+        const {searchName,searchType,data}=this.state
+        //正则匹配
+        const req = eval(`/${searchName}/`)
+        const result=[];
+        console.log(searchName,searchType)
+        if(searchName){
+            if(searchType==="0"){
+                data.forEach(item => {
+                    if(req.test(item.name)){
+                        result.push(item);
+                    }
+                })
+                this.setState({data:result})
+            }else if(searchType==="1"){
+                data.forEach(item => {
+                    if(req.test(item.miaoshu)){
+                        result.push(item);
+                    }
+                })
+                this.setState({data:result})
+            }
+        }else {
+            this.setState({
+                data:this.state.redata
+            })
+        }
+
+    }
+    //下架
+    onSearchRm= (product) =>{
+        const {data,redata}=this.state;
+        const result=[];
+        data.forEach((item,index)=>{
+            if(index==product.key-1){
+                if(product.datenow=="1"){
+                    item.datenow="0"
+                }else{
+                    item.datenow="1"
+                }
+            }
+            result.push(item);
+        })
+        this.setState({
+            data:result,
+            redata:result
+        })
+
+
     }
 
+    componentWillMount() {
+        this.getProduct()
+    }
     render() {
-        //表单获取数据函数
-        function onChange(value) {
-            console.log(`selected ${value}`);
-        }
 
-        function onSearch(val) {
-            console.log('search:', val);
-        }
-        const onFinish = values => {
-            console.log('Success:', values);
-        };
 
         //表格配置
         const columns = [
@@ -33,111 +214,71 @@ export default class ProductHome extends Component {
             { title: '价格', dataIndex: 'jiage', key: 'jiage' ,width: "10%"},
             {
                 title: '状态',
-                dataIndex: "zhuangtai",
-                key: "zhuangtai",
-                render: ()=><div><Button type="primary">下架</Button> <br/> <span>在售</span></div>
+                dataIndex: "",
+                key: "x",
+                render: (product)=> {
+                   return (
+                        <span>
+                            <Button type="primary" onClick={() => this.onSearchRm(product)}>
+                                {product.datenow == "1" ? "下架" : "上架"}
+                            </Button>
+                            <p>{product.datenow == "1" ? "在售" : "已下架"}</p>
+                        </span>)
+                }
             },
             {
                 title: '操作',
                 dataIndex: '',
                 key: 'x',
-                render: () => <div><a>详情</a> <br/> <a>修改</a></div>
+                render: (product) => {
+                    return (
+                        <a onClick={() => this.props.history.push("/product/detail",product)}>
+                            详情 <br/>
+                            修改</a>
+                    )
+                }
             },
         ];
-        //表格数据
-        const data = [
-            {
-                key: 1,
-                name: 'John Brown',
-                miaoshu: 'New York No. 1 Lake Park',
-                jiage:1888+"$",
-                address: "",
-                description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-            },
-            {
-                key: 2,
-                name: 'Jim Green',
-                miaoshu: 'London No. 1 Lake Park',
-                jiage:1388+"$",
-                address: "",
-                description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
-            },
-            {
-                key: 3,
-                name: 'Not Expandable',
-                miaoshu: 'Jiangsu No. 1 Lake Park',
-                jiage:2888+"$",
-                address: "",
-                description: 'This not expandable',
-            },
-            {
-                key: 4,
-                name: 'Joe Black',
-                miaoshu: 'Sidney No. 1 Lake Park',
-                jiage:6888+"$",
-                address: "",
-                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
-            },
-            {
-                key: 5,
-                name: 'Joe Black',
-                miaoshu: 'Sidney No. 1 Lake Park',
-                jiage:5888+"$",
-                address: "",
-                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
-            },
-            {
-                key: 6,
-                name: 'Joe Black',
-                miaoshu: "Sidney No. 1 Lake Park32",
-                jiage:4888+"$",
-                address: '',
-                description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
-            },
-        ];
+
         //左侧
         const title=(
-                <Form layout="inline" onFinish={onFinish} >
-                      <Item name="sousuo">
-                          <Select
-                            showSearch
-                            style={{ width: 150 }}
-                            Value={this.state.searchType}
-                            optionFilterProp="children"
-                            onChange={onChange}
-                            onSearch={onSearch}
-                            filterOption={(input, option) =>
-                                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                            }>
-                            <Option value="0">按名称搜索</Option>
-                            <Option value="1">按描述搜索</Option>
-                            </Select>
-                      </Item>
-                      <Item  name="guanjianzi">
-                            <Search placeholder="关键字" onSearch={onSearch} style={{ width: 200 , marginLeft:10}} />
-                      </Item>
-                      <Item >
-                            <Button type="primary" style={{marginLeft:10}} htmlType="submit" >搜索</Button>
-                      </Item>
-                </Form>
+            <span>
+              <Select
+                defaultValue="0"
+                style={{ width: 150 }}
+                onChange={value => this.setState({searchType:value})}
+                >
+                <Option value="0">按名称搜索</Option>
+                <Option value="1">按描述搜索</Option>
+                </Select>
+              <Input placeholder="关键字"
+                     style={{ width: 200 , marginLeft:10}}
+                     onChange={event => this.setState({ searchName:event.target.value})}
+                     onPressEnter={this.onSearchUp}
+              />
+              <Button type="primary" style={{marginLeft:10}} onClick={this.onSearchUp} >搜索</Button>
+            </span>
             )
         //右侧
         const extra = (
-                <Button type="primary" icon={<PlusOutlined />}>
+                <Button
+                    type="primary"
+                    icon={<PlusOutlined />}
+                    onClick={() => this.props.history.replace("/product/addupdate")}
+                >
                             添加商品
-                        </Button>
+                </Button>
         )
 
 
         return (
             <Card title={title}  extra={extra} style={{ width: "100%" } }>
                 <Table
-                    rowKey="Key"
                     columns={columns}
-                    dataSource={data}
+                    dataSource={this.state.data}
                     bordered={true}
                     pagination={{
-                        defaultPageSize:3,
+                        defaultPageSize:4,
                         showQuickJumper:true,
                     }}
                 />
